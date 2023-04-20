904b912c137d270c6188a33a8bdad8d6befcc2f7
DESKTOP-L4IT8DB\Antonio <limaantoniocarlos20@gmail.com>
2023-04-20 10:18:52 -0300

    validation vs coupling

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -14,7 +14,7 @@ describe("Product unit tests", () => {
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "P1", -1);
-    }).toThrowError("product: Price must be greater than zero");
+    }).toThrowError("product: price must be a positive number");
   });
 
   it("should throw error when name and id are empty", () => {
